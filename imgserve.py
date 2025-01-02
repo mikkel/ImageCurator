97512bd91152e377cd8aa5d5d9976f5b928a24a0
mikkel <mikkel@255bits.com>
2025-01-01 19:43:41 -0700

    show image count

--- imgserve.py
+++ imgserve.py
@@ -4,14 +4,14 @@ import glob
 import shutil
 import sys
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/assets', static_folder="assets")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     src_dir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
     dst_dir = sys.argv[2] if len(sys.argv) > 2 else "keep"
-    upscale_src_dir = sys.argv[3] if len(sys.argv) > 3 else None
-    upscale_dst_dir = sys.argv[4] if len(sys.argv) > 4 else None
+    
+    print("CWD",os.getcwd())
     print("SRC", src_dir)
     print("DST", dst_dir)
 
@@ -23,30 +23,42 @@ def home():
             print("KEEP")
             file_prefix = os.path.splitext(img_name)[0]
 
+
             for file_name in glob.glob(f"{src_dir}/{file_prefix}.*"):
-                shutil.move(file_name, dst_dir)
-                print("MV", file_name, dst_dir)
-            if upscale_dst_dir is not None and upscale_dst_dir is not None:
-                for file_name in glob.glob(f"{upscale_src_dir}/{file_prefix}.*"):
-                    shutil.move(file_name, upscale_dst_dir)
-                    print("MV", file_name, upscale_dst_dir)
+                base_name = os.path.basename(file_name)
+                name, ext = os.path.splitext(base_name)
+
+                # Check and rename if file already exists
+                counter = 1
+                new_name = f"{dst_dir}/{name}{ext}"
+                while os.path.exists(new_name):
+                    new_name = f"{dst_dir}/{name}({counter}){ext}"
+                    counter += 1
+
+                shutil.move(file_name, new_name)
+                print("MV", file_name, new_name)
+            return ""
         elif delete_img:
             print("DELETE")
             file_prefix = os.path.splitext(delete_img)[0]
 
             for file_name in glob.glob(f"{src_dir}/{file_prefix}.*"):
                 os.remove(file_name)
+            return ""
 
         return redirect(url_for('home'))
 
     img_files = sorted([os.path.join('/', f) for f in os.listdir(src_dir) if f.endswith('.png')], key=lambda x: os.path.getmtime(src_dir + x))
-    return render_template('index.html', img_files=img_files)
+    image_count = len(img_files)
+
+    return render_template('index.html', img_files=img_files, image_count=image_count)
 
 @app.route('/<path>.png')
 def send_img(path):
     src_dir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
-    return send_from_directory(src_dir, path+".png")
+    print("SEND", os.getcwd()+"/"+src_dir+"/"+path+".png", os.path.exists(os.getcwd()+"/"+src_dir+"/"+path+".png"))
+    return send_from_directory(os.getcwd()+"/"+src_dir, path+".png")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8666, debug=True)
+    app.run(host='0.0.0.0', port=8667, debug=True)
 
